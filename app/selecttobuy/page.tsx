e65187e4dfa9cee5de9a50953be53cae1c80cb0a
Muhammad Ijaz <epassionate01@gmail.com>
2026-03-15 07:55:34 +0500

    Add select to buy from landing pages

---
+++ app/selecttobuy/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SlectToBuy() {
+  return (
+    <div>SlectToBuy</div>
+  )
+}
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -24,6 +24,15 @@ export function MainNav() {
         Blog
       </Link>
       <Link
+        href="/selecttobuy"
+        className={cn(
+          "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
+          pathname === "/blog" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        Select To Buy
+      </Link>
+      <Link
         href="/about"
         className={cn(
           "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
